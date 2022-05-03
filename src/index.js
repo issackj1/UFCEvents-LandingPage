9226352aeae04637be0bb7f85b2e625996fae738
issackj1 <issack.john1@ucalgary.ca>
2022-05-02 22:26:47 -0600

    move deps

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,6 +6,7 @@ import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 import { createRoot } from "react-dom/client";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 
